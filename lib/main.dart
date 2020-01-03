4145a7091174b578c56e857300a5f187dd9d0ec6
the-whiteman <36057069+wyatthoffman4392@users.noreply.github.com>
2020-01-03 00:13:48 -0700

    Meals and Navigation Added.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,10 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-import './categories_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/category_meals_screen.dart';
+import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,7 +33,19 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      home: TabsScreen(),
+      routes: {
+        CategoryMealsScreen.routeName : (ctx) => CategoryMealsScreen(), 
+        MealDetailScreen.routeName : (ctx) => MealDetailScreen(), 
+      },
+      onGenerateRoute: (settings) {
+        // print(settings.arguments);
+        // return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
+      },
+      // Good for returning an ERROR Page
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen(),);
+      },
     );
   }
 }
